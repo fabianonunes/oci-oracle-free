b4827a7c4e4f5403a751fb34fff5ef8804d458a6
fabianonunes <fparente@senado.leg.br>
2025-09-15 09:20:41 -0300

    separa etapa de configuração para favorcer o cache do docker

---
+++ configureDatabase.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+set -Eeuo pipefail
+
+##################################
+###### Configure Database ########
+##################################
+
+# Set 'oracle' user home directory to ${ORACE_BASE}
+usermod -d "${ORACLE_BASE}" oracle
+
+# Set 'oracle' user password to 'oracle'
+echo "oracle" | passwd --stdin oracle
+
+# Add listener port and skip validations to conf file
+sed -i "s/LISTENER_PORT=/LISTENER_PORT=1521/g" /etc/sysconfig/oracle-free-23*.conf
+sed -i "s/SKIP_VALIDATIONS=false/SKIP_VALIDATIONS=true/g" /etc/sysconfig/oracle-free-23*.conf
+sed -i "s/CHARSET=.*/CHARSET=WE8ISO8859P1/g" /etc/sysconfig/oracle-free-23*.conf
+
+# Disable netca to avoid "No IP address found" issue
+mv "${ORACLE_HOME}"/bin/netca "${ORACLE_HOME}"/bin/netca.bak
+ln -s /bin/true "${ORACLE_HOME}"/bin/netca 
+
+echo "BUILDER: configuring database"
+
+# Set random password
+ORACLE_PASSWORD=$(date '+%s' | sha256sum | base64 | head -c 8)
+# Configure the Oracle Database instance
+(echo "${ORACLE_PASSWORD}"; echo "${ORACLE_PASSWORD}";) | /etc/init.d/oracle-free-23* configure
+
+# Stop unconfigured listener
+su -p oracle -c "lsnrctl stop"
+
+# Re-enable netca
+mv "${ORACLE_HOME}"/bin/netca.bak "${ORACLE_HOME}"/bin/netca
--- install.23.9.sh
+++ install.23.9.sh
@@ -47,43 +47,6 @@ chmod u+w /etc/sudoers
 echo "oracle   ALL=(ALL)   NOPASSWD: ALL" >> /etc/sudoers
 chmod u-w /etc/sudoers
 
-##############################################
-###### Install and configure Database ########
-##############################################
-
-echo "BUILDER: installing database binaries"
-
-# Set 'oracle' user home directory to ${ORACE_BASE}
-usermod -d "${ORACLE_BASE}" oracle
-
-# Set 'oracle' user password to 'oracle'
-echo "oracle" | passwd --stdin oracle
-
-# Add listener port and skip validations to conf file
-sed -i "s/LISTENER_PORT=/LISTENER_PORT=1521/g" /etc/sysconfig/oracle-free-23*.conf
-sed -i "s/SKIP_VALIDATIONS=false/SKIP_VALIDATIONS=true/g" /etc/sysconfig/oracle-free-23*.conf
-sed -i "s/CHARSET=.*/CHARSET=WE8ISO8859P1/g" /etc/sysconfig/oracle-free-23*.conf
-
-# Disable netca to avoid "No IP address found" issue
-mv "${ORACLE_HOME}"/bin/netca "${ORACLE_HOME}"/bin/netca.bak
-echo "exit 0" > "${ORACLE_HOME}"/bin/netca
-chmod a+x "${ORACLE_HOME}"/bin/netca
-
-echo "BUILDER: configuring database"
-
-# Set random password
-ORACLE_PASSWORD=$(date '+%s' | sha256sum | base64 | head -c 8)
-# Configure the Oracle Database instance
-(echo "${ORACLE_PASSWORD}"; echo "${ORACLE_PASSWORD}";) | /etc/init.d/oracle-free-23* configure
-
-# Stop unconfigured listener
-su -p oracle -c "lsnrctl stop"
-
-# Re-enable netca
-mv "${ORACLE_HOME}"/bin/netca.bak "${ORACLE_HOME}"/bin/netca
-
-echo "BUILDER: post config database steps"
-
 ############################
 ### Create network files ###
 ############################
@@ -188,6 +151,7 @@ echo "${OCI_IMAGE_FLAVOR}"  > /etc/oci-image-flavor
 # Perform further Database setup operations
 echo "BUILDER: changing database configuration and parameters for all images"
 su -p oracle -c "sqlplus -s / as sysdba" << EOF
+   STARTUP;
 
    -- Exit on any errors
    WHENEVER SQLERROR EXIT SQL.SQLCODE
