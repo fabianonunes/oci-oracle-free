1e5737ce11d816f6bda349d1eadef51410ae05a1
Fabiano Parente <fabiano@nunes.me>
2025-09-15 21:34:07 -0300

    ajusta diretivas de resize

--- install.23.9.sh
+++ install.23.9.sh
@@ -31,8 +31,8 @@ BUILD_MODE=${1:-"REGULAR"}
 echo "BUILDER: BUILD_MODE=${BUILD_MODE}"
 
 # Set data file sizes (only executed for REGULAR and SLIM)
-USERS_SIZE=10
-TEMP_SIZE=10
+USERS_SIZE=64
+TEMP_SIZE=64
 # Overwrite REGULAR with SLIM sizes
 if [ "${BUILD_MODE}" == "SLIM" ]; then
   REDO_SIZE=10
